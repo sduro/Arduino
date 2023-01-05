2c901d3081e1de5a6d05221f0f30b5070b49077a
Sergi <sergiduro@gmail.com>
2023-01-05 11:42:43 +0100

    Add files via upload

---
+++ status.ino
@@ -0,0 +1,59 @@
+/*
+ Skype status light
+
+ Circuit: Common-cathode LEDs wired like so:
+ * Green anode: digital pin 3
+ * Yellow anode: digital pin 5
+ * Red anode: digital pin 6
+ * Cathode: GND
+
+ Based on example ReadASCIIString by Tom Igoe
+ */
+
+// pins for the LEDs:
+                        // Board   |  Original 
+const int greenPin = 12; //  12 pin |  greenPin = 3; 
+const int yellowPin = 11; // 11 pin |  yellowPin = 5
+const int redPin = 13; //    13 pin |  redPin = 6
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // make the pins outputs:
+  pinMode(greenPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+
+}
+
+void loop() {
+  // if there's any serial available, read it:
+  while (Serial.available() > 0) {
+
+    // look for the next valid integer in the incoming serial stream:
+    int green = Serial.parseInt();
+    // do it again:
+    int yellow = Serial.parseInt();
+    // do it again:
+    int red = Serial.parseInt();
+
+    // look for the newline. That's the end of your
+    // sentence:
+    if (Serial.read() == '\n') {
+      // constrain the values to 0 - 255
+      green = constrain(green, 0, 255);
+      red = constrain(red, 0, 255);
+      yellow = constrain(yellow, 0, 255);
+      
+      // fade the green, yellow, and red LEDs:
+      analogWrite(greenPin, green);
+      analogWrite(redPin, red);
+      analogWrite(yellowPin, yellow);
+
+      // print the three numbers in one string as hexadecimal:
+      Serial.print(green, HEX);
+      Serial.print(yellow, HEX);
+      Serial.println(red, HEX);
+    }
+  }
+}
